1e2c0ad038cede222f49ac0efd1cc5c67eb6b5cd
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-04 16:56:58 -0700

    pokedex

--- Pokedex1/frontend/pokedex.jsx
+++ Pokedex1/frontend/pokedex.jsx
@@ -5,17 +5,30 @@ import {requestAllPokemon} from './actions/pokemon_actions';
 import selectAllPokemon from './reducers/selectors';
 import {fetchAllPokemon} from './util/api_util';
 import configureStore from './store/store';
-import Root from './components/root';
+import { HashRouter, Route } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import PokemonIndexContainer from './components/pokemon/pokemon_index_container';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <HashRouter>
+      <div>
+      <Route path="/" component={PokemonIndexContainer} />
+      </div>
+    </HashRouter>
+  </Provider>
+)
 
 document.addEventListener('DOMContentLoaded', () => {
+
   const rootEl = document.getElementById('root');
   const store = configureStore();
-  window.fetchAllPokemon = fetchAllPokemon;
-  window.receiveAllPokemon = receiveAllPokemon;
-  window.requestAllPokemon = requestAllPokemon;
-  window.selectAllPokemon = selectAllPokemon;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.fetchAllPokemon = fetchAllPokemon;
+  // window.receiveAllPokemon = receiveAllPokemon;
+  // window.requestAllPokemon = requestAllPokemon;
+  // window.selectAllPokemon = selectAllPokemon;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 
   ReactDOM.render(
     <Root store = {store}/>,
